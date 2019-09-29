20e3222a917e232267a8d4403a38b4149c516a99
YoungCapta1n <huzhuhuan1991@gmail.com>
2019-09-29 13:18:17 +0800

    ode

---
+++ common/math/solvers/ode/include/odesolver.h
@@ -0,0 +1,18 @@
+/*
+***********************************************************************
+* odesolver.h:
+* ode solver with support to Eigen
+* This header file can be read by C++ compilers
+*
+* by Hu.ZH(CrossOcean.ai)
+***********************************************************************
+*/
+
+#ifndef _ODESOLVER_H_
+#define _ODESOLVER_H_
+
+#include <boost/numeric/odeint/external/eigen/eigen.hpp>
+#include <boost/numeric/odeint/integrate/integrate.hpp>
+#include <boost/test/unit_test.hpp>
+
+#endif /* _ODESOLVER_H_ */
